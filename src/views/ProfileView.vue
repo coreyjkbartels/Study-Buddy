3f8a9ba43cc9ba2044eaab5962580d2cffa0dff5
Dylan Kossoy <dylankossoy@Dylans-MacBook-Pro-9.local>
2025-10-21 12:49:43 -0400

    update folders

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,15 +1,53 @@
+<script setup>
+
+
+</script>
+
 <template>
-  <div class="page">
-    <h1>Profile</h1>
+  <div class="main-container">
+    <div class="header-container">
+      <button class="back-button"></button>
+    </div>
+    <form action="" class="form-container">
+
+    </form>
+
+
   </div>
 </template>
 
 <style scoped>
-.page {
+
+
+.header-container {
+  width: 100vw;
+  height: 70px;
+  background: black;
+  position: absolute;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+
+}
+
+.back-button {
+  margin-left: 4rem;
+}
+
+.form-container {
+  width: 800px;
+  height: 600px;
+  background: blue;
+}
+
+.main-container {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  background: #c5cae9;
+  background: rgb(229, 229, 229);
+  position: relative;
+
 }
 </style>
